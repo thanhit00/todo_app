b659fad86108eee151f93d84c483f228d6e8d841
thanhit00 <ducthanhndtpv@gmail.com>
2022-01-22 10:05:53 +0700

    completed

--- lib/ui/notified_page.dart
+++ lib/ui/notified_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:get/get.dart';
 class NotifiedPage extends StatelessWidget {
   final String? label;
